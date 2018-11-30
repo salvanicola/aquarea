a8d4a4e84d8915ec0ae88702b2f1df5566d030f3
Manuv46 <manuel.vr46@gmail.com>
2018-11-30 11:22:13 +0100

    correzione cose
    create nuove tabella messaggi e coretta relazione utente persona

--- sql/aquareadb.sql
+++ sql/aquareadb.sql
@@ -9,7 +9,8 @@ Nazione CHAR(50) NOT NULL,
 CAP INT(5) NOT NULL,
 Via VARCHAR(50) NOT NULL,
 Telefono VARCHAR(10) NOT NULL,
-Mail VARCHAR(50) NOT NULL UNIQUE REFERENCES Utente(Email)
+Mail VARCHAR(50) UNIQUE, 
+FOREIGN KEY (Mail) REFERENCES Utente(Email)
 );
 
 CREATE TABLE IF NOT EXISTS Piscina(
@@ -26,7 +27,11 @@ CREATE TABLE IF NOT EXISTS Utente(
 Email VARCHAR (50) PRIMARY KEY,
 Username VARCHAR (20) NOT NULL UNIQUE,
 Password VARCHAR (50) NOT NULL,
-Abbonamento ENUM('Si','No') NOT NULL,
+Abbonamento ENUM('Si','No') NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Messaggi(
+Email VARCHAR (50) NOT NULL REFERENCES Utente(Email),
 Messaggi VARCHAR (200)
 );
 
@@ -177,6 +182,22 @@ END
 //
 DELIMITER ;
 
+DROP TRIGGER IF EXISTS CHK_Abbonamento2;
+DELIMITER //
+CREATE TRIGGER CHK_Abbonamento2 AFTER INSERT ON Utente
+FOR EACH ROW
+BEGIN
+	DECLARE T VARCHAR(50);
+	IF EXISTS(SELECT * FROM Persona P INNER JOIN Utente U ON P.Mail=U.Email WHERE U.Abbonamento='No') THEN
+    SET T=(SELECT Mail FROM Persona P INNER JOIN Utente U ON P.Mail=U.Email WHERE U.Abbonamento='No');
+    UPDATE Utente
+    SET Abbonamento='Si'
+    WHERE T=Email;
+    END IF;
+END
+//
+DELIMITER ;
+
 DROP PROCEDURE IF EXISTS Ingresso;
 DELIMITER //
 CREATE PROCEDURE Ingresso (usern VARCHAR(50), pass VARCHAR(50))
@@ -184,7 +205,7 @@ BEGIN
 	DECLARE msg VARCHAR(200);
 	IF (SELECT COUNT(*) FROM  Utente WHERE ((Email LIKE usern) OR (Username LIKE usern)) AND (Password LIKE pass))>0 THEN
 		IF (SELECT Abbonamento FROM  Utente WHERE ((Email LIKE usern) OR (Username LIKE usern)) AND (Password LIKE pass)) = 'NO' THEN
-			SELECT Messaggi FROM  Utente WHERE ((Email LIKE usern) OR (Username LIKE usern)) AND (Password LIKE pass);
+			SELECT M.Messaggi FROM  Utente U INNER JOIN Messaggi M ON U.Email=M.Email WHERE ((U.Email LIKE usern) OR (U.Username LIKE usern)) AND (U.Password LIKE pass);
 		ELSE
 			SELECT P.Nome AS NomeP, P.Cognome AS CognomeP, P.Telefono, I.Inizio, I.Fine, C.NomeCorso, C.Costo
 			FROM Utente U, Persona P, Iscritti I INNER JOIN Corso C ON I.CodCorso=C.CodiceC
@@ -203,7 +224,7 @@ DELIMITER //
 CREATE PROCEDURE Inserimento_Utente(IN Mail VARCHAR (50), IN username VARCHAR (20), IN password VARCHAR (50))
 BEGIN 
 		INSERT INTO Utente 
-        VALUES (Mail, username, password, 'No', NULL);
+        VALUES (Mail, username, password, 'No');
 END
 //
 DELIMITER ;
@@ -218,6 +239,15 @@ END
 //
 DELIMITER ;
 
+DROP PROCEDURE IF EXISTS Inserimento_Messaggi;
+DELIMITER //
+CREATE PROCEDURE Inserimento_Messaggi(Mail VARCHAR (50), Messaggio VARCHAR (200))
+BEGIN	
+	INSERT INTO Messaggi
+    VALUES (Mail, Messaggio);
+END
+//
+DELIMITER ;
 
 
 
