dbfe9b1b5e5720621ddeefbe2704b74c159a9037
salvanicola <nicola.salvadore@gmail.com>
2019-01-12 10:42:09 +0100

    menu e js

--- js/scriptsidebar.js
+++ js/scriptsidebar.js
@@ -5,3 +5,11 @@ function openNav() {
 function closeNav() {
   document.getElementById("header").style.width = "0";
 }
+
+function openMen() {
+  document.getElementById("piscina").style.display = "block";
+}
+
+function closeMen() {
+  document.getElementById("overlay").style.display = "none";
+}
