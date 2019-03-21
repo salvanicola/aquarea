a3063e180657f1f06de2918a7fba7708441bfe75
salvanicola <nicola.salvadore@gmail.com>
2019-03-21 18:31:08 +0100

    21/03 manu gay

--- js/scriptsidebar.js
+++ js/scriptsidebar.js
@@ -1,9 +1,13 @@
 function openNav() {
   document.getElementById("header").style.width = "100%";
+  document.getElementById("headerformobile").style.display = "none";
+  document.getElementById("imglogo").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("header").style.width = "0";
+  document.getElementById("headerformobile").style.display = "initial";
+  document.getElementById("imglogo").style.display = "initial";
 }
 
 function openMen() {
@@ -13,8 +17,12 @@ function openMen() {
   for (i = 0; i < dropdowns.length; i++) {
     var openDropdown = dropdowns[i];
     if (openDropdown.classList.contains('show') && openDropdown.getAttribute('id')!='piscina') {
+		//openDropdown.parentElement.style.paddingBottom = "8%";
       openDropdown.classList.remove('show');
     }
+	//else if(openDropdown.getAttribute('id')=='piscina'){
+		//openDropdown.parentElement.style.paddingBottom = "0";
+	//}
   }
 }
 
@@ -25,7 +33,20 @@ function openMen2(){
   for (i = 0; i < dropdowns.length; i++) {
     var openDropdown = dropdowns[i];
     if (openDropdown.classList.contains('show') && openDropdown.getAttribute('id')!='palestra') {
+		//openDropdown.parentElement.style.paddingBottom = "8%";
       openDropdown.classList.remove('show');
+	  
     }
   }
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("headerformobile").style.top = "0";
+  } else {
+    document.getElementById("headerformobile").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
 }
