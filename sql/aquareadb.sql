a37abc3c03c2e9225881cb47b6dc327fde08c310
Manuv46 <manuel.vr46@gmail.com>
2019-07-06 12:14:46 +0200

    server popolato
    bella

--- sql/aquareadb.sql
+++ sql/aquareadb.sql
@@ -72,4 +72,18 @@ DELIMITER ;
 
 INSERT INTO users (email, username, password, user_type) 
         VALUES ('admin@stuendi.unipd.it','admin', MD5('admin'),'Admin'),
-			   ('user@stuendi.unipd.it','user', MD5('user'),'Mod');
+			   ('user@stuendi.unipd.it','user', MD5('user'),'Mod'),
+			   ('franco@gmail.it','franco', MD5('franco'),'Mod'),
+			   ('beppi@gmail.it','beppi', MD5('beppi'),'Mod'),
+			   ('carlo@gmail.it','carlo', MD5('carlo'),'Mod');
+
+INSERT INTO news (title, content, author, data, img)
+		VALUES ('Benvenuti sul sito ', 'di Aquarea Vicenza', 'admin', '19/07/03', 'news1.jpg'),
+			   ('il nostro nuovo sito', 'potete trovare tutte le informazioni riguardanti le strutture, orari dei corsi, 
+			   inviare curriculum per entrare a far parte del nostro staff', 'user', '19/07/03', 'news2.jpg'),
+			   ('Seuguici su Facebook', 'per non perdierti nuove offerte di corsi e tanto altro', 'user', '19/07/06', 'ec29cabf47ff3fc7b738e4591bcc6e77.jpg');
+			   
+INSERT INTO requests (name, surname, date, email, sesso, note, cv)
+		VALUES ('Andrea', 'Schiavo', '98/03/16', 'andreaschiavo@gmail.com', 'Maschio', '', 'cv1.pdf'),
+		       ('Marco', 'Dal Toso', '95/09/21', 'daltosomarco@gmail.com', 'Maschio', '', 'cv2.pdf'),
+			   ('Silvia', 'Marcon', '97/05/03', 'silviamarcon@gmail.com', 'Femmina', '', 'cluedo.pdf');
